dfee3bf610e44ea7852fb25634c8dd6f87315726
Tiago Leal <tiago.sleal12@gmail.com>
2019-05-02 22:44:52 -0300

    create screens categories

--- src/screens/restaurants/index.js
+++ src/screens/restaurants/index.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
 import ListRestaurants from "../../components/list_restaurants";
+import Categories from "../../components/categories";
 
 const Restaurants = () => (
   <Fragment>
+    <Categories />
     <ListRestaurants />
   </Fragment>
 );
