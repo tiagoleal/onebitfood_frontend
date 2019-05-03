48b70fa2db91a34e88c3eef750aff9cc99755f84
Tiago Leal <tiago.sleal12@gmail.com>
2019-05-02 22:27:08 -0300

    create the component restaurant

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Column, Button, Icon, Title } from "rbx";
 import { FaCrosshairs } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import "../../styles/home.scss";
 
@@ -10,12 +11,14 @@ const Home = () => (
       <Title size={2}>Comida saudável e gostosa</Title>
       <Title subtitle size={3}>direto na sua casa rapidamente</Title>
       <Title subtitle size={5}>Conheça novos sabores</Title>
-      <Button size="medium" color="custom-orange">
-        <Icon size="small" color="white">
-          <FaCrosshairs />
-        </Icon>
-        <span class="has-text-white">Encontrar Agora</span>
-      </Button>
+      <Link to="/restaurants">
+        <Button size="medium" color="custom-orange">
+          <Icon size="small" color="white">
+            <FaCrosshairs />
+          </Icon>
+          <span class="has-text-white">Encontrar Agora</span>
+        </Button>
+      </Link>
     </Column>
   </Column.Group>
 )
