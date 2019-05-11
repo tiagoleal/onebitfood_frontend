12b4ca671746b7d4a9442d3c01bb514fd2193326
Tiago Leal <tiago.sleal12@gmail.com>
2019-05-11 16:44:36 -0300

    setup de redux persist to th local cache

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import Reducers from "../reducers";
-
-export default createStore(Reducers, applyMiddleware(thunk));
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+ 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, Reducers)
+ 
+export default createStore(
+  persistedReducer, 
+  composeEnhancer(applyMiddleware(thunk))
+);
