a80e46b443d386f054289a15b7d163c4c40ab044
Tiago Leal <tiago.sleal12@gmail.com>
2019-05-12 20:04:58 -0300

    development the total order

--- src/actions/new_order.js
+++ src/actions/new_order.js
@@ -1,5 +1,6 @@
 import { ADD_ORDER_ITEM } from "./action_types";
- 
+import { REMOVE_ORDER_ITEM } from "./action_types";
+
 export const addOrderItem = (restaurant, product, quantity, comment) => {
   return {
     type: ADD_ORDER_ITEM,
@@ -7,5 +8,13 @@ export const addOrderItem = (restaurant, product, quantity, comment) => {
     product: product,
     quantity: quantity,
     comment: comment
+  }
+}
+
+
+export const removeOrderItem = (orderItem) => {
+  return {
+    type: REMOVE_ORDER_ITEM,
+    orderItem: orderItem
   }
 }
