7d97d1f1453af19ee6e492dcd1a7d5440fd67ea4
Tiago Leal <tiago.sleal12@gmail.com>
2019-05-12 19:33:45 -0300

    create the shopping cart component

--- src/components/category_products/index.js
+++ src/components/category_products/index.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { Title, Column, Box, Image } from 'rbx';
+ 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { showModal } from "../../actions/modal";
+ 
 import "../../styles/product.scss";
+ 
 
 const CategoryProducts = (props) => (
   <div id="product">
     <Title size="5">{props.title}</Title>
     <Column.Group gapSize={2} multiline >
-      { props.products.map((product, i) => {
-        return <Column size="6" key={i}>
-                <Box>
+      {props.products.map((product, i) => {
+        return (
+            <Column key={i} size="4">
+              <Box>
+                <a href="#" onClick={() => props.showModal('ADD_PRODUCT', {'product': product, 'restaurant': props.restaurant})}>
                   <Column.Group>
-                    <Column size="three-fifths">
+                    <Column size="6" offset={1}>
                       <Title size="6">{product.name}</Title>
-                      <p>{product.description}</p>
+                      <Title subtitle size="6">{product.description}</Title>
                       <span className="dashed_box">Preço ${product.price}</span>
                     </Column>
                     <Column size="two-fifths">
                       <Image src={product.image_url} width="50%" />
                     </Column>
                   </Column.Group>
-                </Box>
-              </Column>
+                </a>
+              </Box>
+            </Column>
+        )
       })}
     </Column.Group>
   </div>
 );
 
-export default CategoryProducts;
+const mapDispatchToProps = dispatch => bindActionCreators({ showModal}, dispatch);
+export default connect(null, mapDispatchToProps)(CategoryProducts);
